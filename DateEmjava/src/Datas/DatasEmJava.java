becb3017328256b6ff0edf76f5612d0e418b9be6
romar <romar@192.168.1.3>
2022-03-03 10:23:37 -0300

    Tipos

--- DateEmjava/src/Datas/DatasEmJava.java
+++ DateEmjava/src/Datas/DatasEmJava.java
@@ -7,7 +7,20 @@ public class DatasEmJava {
 	public static void main(String[] args) {
 		
 		Date date = new Date();
-		System.out.println(date);
+		System.out.println("//A data em milisegundos "+date.getTime());
+		
+		System.out.println("Dia do mês "+date.getDate());
+		
+		System.out.println("Dia da semana "+date.getDay());
+		
+		System.out.println("Hora do dia "+date.getHours());
+		
+		System.out.println("Minutos da hora "+date.getMinutes());
+		
+		System.out.println("Segundos "+date.getSeconds());
+		
+		
+		System.out.println("Ano "+(date.getYear()+1900));
 	}
 	
 	
